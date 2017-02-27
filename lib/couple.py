a4c0c81944a2b8fad3f4021db8431e558c836cb7
Benoit Valot <benoit.valot@univ-fcomte.fr>
2017-02-27 13:14:39 +0100

    Add filter of overlapping couple

--- lib/couple.py
+++ lib/couple.py
@@ -8,7 +8,8 @@
 
 class Couple():
     """Class containing a start and end clip position with potential IS"""
-    def __init__(self, posstart, posend):
+    def __init__(self, chrom, posstart, posend):
+        self.chrom = chrom
         self.posstart = posstart
         self.posend = posend
         self.consstart = None
@@ -23,6 +24,9 @@ class Couple():
         if self.consstart is None or self.consend is None:
             self.createconsensus()
         ##TODO search ir on consensus
+
+    def rangeposition(self):
+        return range(self.posstart.pos, self.posend.pos+1)
 
     def __str__(self):
         return str(self.posstart) + "\t" + str(self.posend)
@@ -40,7 +44,8 @@ class Couples():
         self.couples = []
 
     def groupeposition(self, positions, size):
-        """Fonction to parse the positions and regroup start and end clip positions to a couple"""
+        """Fonction to parse the positions 
+        and regroup start and end clip positions to a couple"""
         posstart = []
         posend = []
         chrom = None
@@ -53,29 +58,67 @@ class Couples():
                 if len(pos.clipend) != 0:
                     posend.append(pos)
             else: ##change chromosome, create couple and reset
-                self.__createcouples(posstart, posend, size)
+                self.__createcouples(chrom,posstart, posend, size)
                 posstart = []
                 posend = []
                 chrom = pos.chrom
         ##add last chromomose couple
-        self.__createcouples(posstart, posend, size)
+        self.__createcouples(chrom, posstart, posend, size)
 
     
-    def __createcouples(self, posstart, posend, size):
+    def __createcouples(self, chrom, posstart, posend, size):
         """Create all couple from same chromomosome"""
         for s in posstart:
+            if s.chrom != chrom:
+                raise Exception("Only position with same chromosome can be coupled")
             for e in posend:
+                if e.chrom != chrom:
+                    raise Exception("Only position with same chromosome can be coupled")
                 diff = (e.pos - s.pos)
                 if diff >= 0 and diff <= size:
-                    self.couples.append(Couple(s,e))
+                    self.couples.append(Couple(chrom,s,e))
 
     def filteroverlapcouple(self):
         """Function to filter overlap couples and return best couple"""
         couplestmp = []
+        cur_couple = None
         for couple in self.couples:
-            couplestmp.append(couple)
+            if cur_couple is None:
+                cur_couple = couple
+            else:
+                if self.__overlap(couple, cur_couple):
+                    cur_couple = self.__best(couple, cur_couple)
+                else:
+                    couplestmp.append(cur_couple)
+                    cur_couple = couple
+        if cur_couple is not None:
+            couplestmp.append(cur_couple)
         self.couples = couplestmp
 
+    def __overlap(self, couple1, couple2):
+        if couple1.chrom != couple2.chrom:
+            return False
+        else:
+            a = set(couple1.rangeposition())
+            b = set(couple2.rangeposition())
+            if len(a.intersection(b)) == 0:
+                return False
+            else:
+                return True
+
+    def __best(self, couple1, couple2):
+        diff = len(couple1.posstart.clipstart) - len(couple2.posstart.clipstart) + \
+               len(couple1.posend.clipend) - len(couple2.posend.clipend)
+        if diff < 0:
+            return couple2
+        elif diff > 0:
+            return couple1
+        else:
+            if len(couple1) > len(couple2):
+                return couple1
+            else:
+                return couple2
+    
     def __iter__(self):
         for couple in self.couples:
             yield couple
