5c996816f0562dbc8216e0f02050cd76a5dc3b54
panisa <panisa.por@gmail.com>
2017-03-09 19:08:37 +0100

    Find the best IR by length

--- lib/invertedrepeat.py
+++ lib/invertedrepeat.py
@@ -7,16 +7,15 @@
 ##Licence GPL
 
 import subprocess
-import tempfile
-import traceback
+import temporalfile as temp
 
 class InvertRepeat():
     """Class containing inverted repeat at the 5' and the 3'"""
-    def __init__(self, prime5_pos, prime5_seq, prime3_pos, prime3_seq):
-        self.prime5_pos = prime5_pos ##position beginning ir from clip end consensus (1)
-        self.prime5_seq = prime5_seq ##Sequence ir from clip end consensus
-        self.prime3_pos = prime3_pos ##position - len(seq) -1 beginning ir from clip start consensus (-1)
-        self.prime3_seq = prime3_seq ##sequence ir from clip start consensus (reverse)
+    def __init__(self):
+        self.prime5_pos = None ##position beginning ir from clip end consensus (1)
+        self.prime5_seq = None ##Sequence ir from clip end consensus
+        self.prime3_pos = None ##position - len(seq) -1 beginning ir from clip start consensus (-1)
+        self.prime3_seq = None ##sequence ir from clip start consensus (reverse)
 
 
     def __str__(self):
@@ -26,99 +25,64 @@ class InvertRepeat():
             return " ".join([str(self.prime5_pos), self.prime5_seq, str(self.prime3_pos), self.prime3_seq])
 
 
+    def extractIRinfo(self, len_consensus, irdata):
+        ##Extract IR information
 
-def createtempfile():
-    newtempfile = tempfile.NamedTemporaryFile()
-    return newtempfile
+        rangeIRpos = 10
+        len_bestir = rangeIRpos*2
 
-        
-def closetempfile(closefile):
-    closefile.close()
+        if len(irdata) > 3:
+            for block in range(len(irdata)/4):#a block refers a result set that contains 4 lines
+                line = 4*block
 
+                prime5_pos_temp = int(irdata[0+line].split('_')[-2])
+                prime5_seq_temp = irdata[1+line].splitlines()[0]
+                prime3_pos_temp = int(irdata[2+line].split('_')[-1])-len_consensus-1
+                prime3_seq_temp = irdata[3+line].splitlines()[0]
 
-def writetempfile(writedata):
-    temp = tempfile.NamedTemporaryFile()
-    try:
-        temp.write(writedata)
-        temp.seek(0)    
-    except Exception:
-        exe = traceback.format_exc()
-        print exe
-    return temp
+                ##Check IR in range
+                if prime5_pos_temp <= rangeIRpos and prime3_pos_temp >= -rangeIRpos :
+                    
+                    ##Find the best IR
+                    if prime5_pos_temp-prime3_pos_temp < len_bestir:
+                        len_bestir = prime5_pos_temp-prime3_pos_temp
+                        self.prime5_pos = prime5_pos_temp
+                        self.prime3_pos = prime3_pos_temp
+                        self.prime5_seq = prime5_seq_temp
+                        self.prime3_seq = prime3_seq_temp
 
-
-def readtempfile(tempread):
-    readdata = []
-    try:
-        tempread.seek(0)
-        readdata = tempread.readlines()
-    except Exception:
-        exe = traceback.format_exc()
-        print exe
-    return readdata
 
 
 def searchir(prime5seq, prime3seq):
-    """Searched ir from 5 to 3 prie consensus and return InvertRepeat class"""
+    """Searched ir from 5 to 3 prime consensus and return InvertRepeat class"""
     ##TODO by super Panisa
     ##Only store IR that is close to the 5 and 3 prime (<10bp)
-
-    rangeIRpos = 10
-    prime5_min = rangeIRpos
-    prime3_max = -rangeIRpos
-    prime5_seq_result = None
-    prime3_seq_result = None
-    len_seq = len(prime5seq+prime3seq)
-
+    
     ##Prepare input for einverted
-    intempfile = writetempfile('\n'.join(['>',prime5seq+prime3seq]))
+    intempfile = temp.writefile("\n".join([">",prime5seq+prime3seq]))
 
-    outtempfile = createtempfile()
-    seqtempfile = createtempfile()
+    outtempfile = temp.createfile()
+    seqtempfile = temp.createfile()
 
     ##Call einverted from emboss to find IR
-    commandir = ['einverted -sequence '+ intempfile.name +' -gap 12 -threshold 15 -match 3 -mismatch -4 -outfile ' + outtempfile.name + ' -outseq '+ seqtempfile.name + ' -auto Y -warning N']
+    commandir = ['einverted -sequence '+ intempfile.name +' -gap 12 -threshold 15 -match 3 -mismatch -4 -outfile ' \
+    + outtempfile.name + ' -outseq '+ seqtempfile.name + ' -auto Y -warning N']
     processir = subprocess.Popen(commandir, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     processir.communicate()
 
     ##Read data from outseq of einverted
-    outseq_data = readtempfile(seqtempfile)
-
-    ##Extract IR information
-    if len(outseq_data) > 3:
-        for block in range(len(outseq_data)/4):
-            line = 4*block
-
-            prime5_pos_temp = int(outseq_data[0+line].split('_')[-2])
-            prime5_seq_temp = outseq_data[1+line].splitlines()[0]
-            prime3_pos_temp = int(outseq_data[2+line].split('_')[-1])-len_seq-1
-            prime3_seq_temp = outseq_data[3+line].splitlines()[0]
-
-            ##Check IR in range
-            if prime5_pos_temp <= rangeIRpos and prime3_pos_temp >= -rangeIRpos :
-                
-                ##Find the best IR
-                if (prime5_pos_temp <= prime5_min) and (prime3_pos_temp >= prime3_max):
-                    prime5_min = prime5_pos_temp
-                    prime3_max = prime3_pos_temp
-                    prime5_seq_result = prime5_seq_temp
-                    prime3_seq_result = prime3_seq_temp
-            else:
-                pass
-    else:
-        pass
+    outseq_data = temp.readfile(seqtempfile)
 
     ##Close all temp files
-    closetempfile(intempfile)
-    closetempfile(outtempfile)
-    closetempfile(seqtempfile)
+    temp.closefile(intempfile)
+    temp.closefile(outtempfile)
+    temp.closefile(seqtempfile)
 
-    ##Return original values of prime5_pos and prime3_pos for "Not found IR" case
-    if prime5_seq_result is None:
-        prime5_min = None
-        prime3_max = None
-    
-    invertRepeat = InvertRepeat(prime5_min, prime5_seq_result, prime3_max, prime3_seq_result)
-
+    invertRepeat = InvertRepeat()
+    invertRepeat.extractIRinfo(len(prime5seq+prime3seq), outseq_data)
 
     return invertRepeat
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
