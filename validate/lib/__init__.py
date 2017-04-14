fdae24945fba0a0ebf05e2f6c4fd9ce77e6f28db
panisa <panisa.por@gmail.com>
2017-04-14 16:00:53 +0200

    init

---
+++ validate/lib/__init__.py
@@ -0,0 +1,8 @@
+"""Library containing IS object
+"""
+
+# Copyright (c) 2017 Benoit Valot and Panisa Treepong
+##benoit.valot@univ-fcomte.fr
+
+# from clipread import ClipRead
+# from position import Position
