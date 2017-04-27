70bd80f97f5041a3d9c01fd0c68e379795382a87
panisa <panisa.por@gmail.com>
2017-04-27 04:37:04 +0200

    manage file and directory

---
+++ validate/util/managefiledir.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+from os import listdir
+
+def makedir(newDir):
+    if not os.path.exists(newDir):
+        os.makedirs(newDir)
+
+def removedir(dirName):
+	shutil.rmtree(dirName,ignore_errors=True)
+
+def rmexceptfile(dirName,fileName):
+    for f in listdir(dirName):
+        if f != fileName:
+            os.remove(dirName+f)
+
+def copyfile(oldDir,newDir):
+	shutil.copyfile(oldDir,newDir)
