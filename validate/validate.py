2f1507665d198073c51f9a7bfdcec58d35261af5
panisa <panisa.por@gmail.com>
2017-05-13 15:35:49 +0200

    add args.size of DR, args.quality and use tempfile

--- validate/validate.py
+++ validate/validate.py
@@ -8,7 +8,9 @@
 
 """Validate panISa [search (IS) insertion on a genome] by simulation read"""
 
+import sys
 import argparse
+import tempfile
 import subprocess
 from os import listdir
 from util import siminsertseq
@@ -18,25 +20,24 @@ from util.genreport import GenReport
 
 desc = "Validate panISa [search (IS) insertion on a genome] by simulation read"
 command = argparse.ArgumentParser(prog='validate.py', \
-    description=desc, usage='%(prog)s [options] fasta_dir isinfo_dir')
-command.add_argument('fasta_dir', action='store', \
-    help='Input directory that store fasta file')
-command.add_argument('isinfo_dir', action='store', \
-    help='Input directory that store info of IS file')
-command.add_argument('-o', '--output_dir', action='store', default='output/',\
-    help='Place all output directories of all subprocess of validation, default=output')
-command.add_argument('-s', '--seq_dir', action='store', default='output/seq/',\
-    help='Return file of the simulated complete genome sequence with ISs, default=seq')
-command.add_argument('-i', '--info_dir', action='store', default='output/info/',\
-    help='Return report of information which insert in simulated complete genome, default=info')
-command.add_argument('-r', '--report_dir', action='store', default='output/report/',\
-    help='Return comparation report between panISa and real IS, default=report')
-command.add_argument('-n', '--number_IS', nargs="?", \
-    type=int, default=30, help='Number of ISs which were randomly added in simulation, default=30')
-command.add_argument('-c', '--coverage', nargs="?", \
-    type=int, default=60, help='Mean coverage for simulation read, default=60')
-command.add_argument('-l', '--length', nargs="?", \
-    type=int, default=150, help='Length of the first and second reads, default=150')
+    description=desc, usage='%(prog)s [options] fasta isinfo')
+command.add_argument('fasta', nargs="?", type=argparse.FileType("r"),\
+    help='Input fasta file')
+command.add_argument('isinfo', nargs="?", type=argparse.FileType("r"),\
+    help='Input tabular file that store information of ISs')
+command.add_argument('-o', '--output', nargs="?", type=argparse.FileType("w"), \
+    default=sys.stdout,\
+    help='Return comparation report between panISa and simulation, default=stdout')
+command.add_argument('-n', '--number_IS', nargs="?", type=int, default=30, \
+    help='Number of ISs which were randomly added in simulation, default=30')
+command.add_argument('-c', '--coverage', nargs="?", type=int, default=60, \
+    help='Mean coverage for simulation read, default=60')
+command.add_argument('-l', '--length', nargs="?", type=int, default=150, \
+    help='Length of the first and second reads, default=150')
+command.add_argument('-s', '--size', nargs="?", type=int, default=15, \
+    help='Maximun size of direct repeat region, default=15')
+command.add_argument('-q', '--quality', nargs="?", type=int, default=20, \
+    help='Min alignment quality value to conserved a clip read, default=20')
 command.add_argument('-v', '--version', action='version', \
     version='%(prog)s 0.1.0')
 
@@ -44,40 +45,39 @@ command.add_argument('-v', '--version', action='version', \
 if __name__=='__main__':
     """Performed job on execution script""" 
     args = command.parse_args()
-    read_dir = '%sread/'%(args.output_dir)
-    temp_dir = '%stemp/'%(args.output_dir) ## will be change to use temp file after be sure this scirpt is correct
+    temp_dir = "temp/"
 
-    ##Prepare directory for all outputs
-    man.makedir(args.output_dir)
-    man.removedir(args.output_dir)
-    ##Prepare directory for simulated read output
-    man.makedir(read_dir)
-    ##Prepare directory for report output
-    man.makedir(temp_dir) ## will be change to use temp file after be sure this scirpt is correct
-    man.makedir(args.report_dir)
-
-    for sim in listdir(args.fasta_dir):
-        ##Simutale the complete genome sequence with ISs
-        simfasta_file = siminsertseq.getSimIS(args.isinfo_dir,args.fasta_dir+sim,args.seq_dir,args.info_dir,args.number_IS)
-
-        ##Move ref file and simulated complete genome as the same place 
-        man.copyfile(args.fasta_dir+sim,read_dir+sim)
-        ref_file = read_dir+sim
-        man.copyfile(simfasta_file,read_dir+simfasta_file.split("/")[-1])
-        simfasta_file = read_dir+simfasta_file.split("/")[-1]
-        ##Simulate read from simulated complete genome
-        simread.simread(ref_file,simfasta_file,args.length,args.coverage,read_dir)
-        ##Remove intermediate files of read simulation process
-        readfile = simfasta_file.split("/")[-1].split(".fasta")[0]+".bam"
-        man.rmexceptfile(read_dir,readfile)
-
-        ##Call panISa.py
-        cmdIS = "../panISa.py %s -o %s%s"%(read_dir+readfile,temp_dir,sim.split(".")[0])
-        subprocess.call(cmdIS, shell=True)
-
-        ##Create comparation report
-        name = sim.split(".fasta")[0]
-        report = GenReport()
-        report.processReport(args.info_dir+name,temp_dir+name,args.report_dir+name)
+    ##[Prepare temp files and temp directory for all outputs]
+    man.makedir(temp_dir) ##[Prepare directory for simulated read output]
+    simisinfo_resultfile = tempfile.NamedTemporaryFile()
+    panisa_resultfile = tempfile.NamedTemporaryFile()
 
 
+    ##[Simulate the complete genome sequence with ISs]
+    simfasta_file = siminsertseq.getSimIS(args.isinfo.name,args.fasta.name, \
+        temp_dir,simisinfo_resultfile.name,args.number_IS)
+
+    ##[Move original fasta file and simulated complete genome as the same place]
+    ref_file = temp_dir+args.fasta.name.split("/")[-1]
+    man.copyfile(args.fasta.name,ref_file)
+
+    ##[Simulate read from simulated complete genome]
+    simread.simread(ref_file,simfasta_file,args.length,args.coverage,temp_dir)
+
+    ##[Remove intermediate files of read simulation process]
+    readfile = simfasta_file.split("/")[-1].split(".fasta")[0]+".bam"
+    man.rmexceptfile(temp_dir,readfile)
+
+    ##[Call panISa.py]
+    cmdIS = "../panISa.py %s -o %s -s %s -q %s"%(temp_dir+readfile,\
+        panisa_resultfile.name,args.size,args.quality)
+    subprocess.call(cmdIS, shell=True)
+
+    ##[Create comparation report]
+    report = GenReport()
+    report.processReport(simisinfo_resultfile.name,panisa_resultfile.name,args.output)
+
+    ##[Clear temp files]
+    simisinfo_resultfile.close()
+    panisa_resultfile.close()
+    man.removedir(temp_dir)
