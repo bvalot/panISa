160eb155d532dfb7e94b25c1342915aa12028930
Benoit Valot <benoit.valot@univ-fcomte.fr>
2018-07-24 15:55:53 +0200

    Push on pypi correction

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.2',  # Required
+    version='0.1.3',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -81,7 +81,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
